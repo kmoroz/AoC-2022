5dac5ab220b63f3936d437379841589be654852b
kmoroz <ksmorozova@hotmail.com>
2022-12-01 23:09:39 +0000

    initial

---
+++ AoC-2022/01/1.cs
@@ -0,0 +1,26 @@
+﻿using AoC_2022;
+
+var path = await Utils.GetInput(1);
+
+List<int> runningSum = new List<int>();
+int runningCount = 0;
+
+foreach(var line in File.ReadAllLines(path))
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        runningSum.Add(runningCount);
+        runningCount = 0;
+    }
+    else
+    {
+        runningCount += int.Parse(line);
+    }
+}
+
+//Part 1
+Console.WriteLine(runningSum.Max());
+
+//Part 2
+var result = runningSum.OrderByDescending(i => i).Take(3).Sum();
+Console.WriteLine(result);
---
+++ AoC-2022/Utils.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+
+namespace AoC_2022
+{
+    public class Utils
+    {
+        public static async Task<string> GetInput(int day)
+        {
+            var fileName = "input.txt";
+            var filePath = GetRelativePath(day, fileName);
+
+            if (!File.Exists(filePath))
+            {
+                var uri = new Uri("https://adventofcode.com");
+                var cookies = new CookieContainer();
+                cookies.Add(uri, new Cookie("session", GetCookie()));
+                using var file = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None);
+                using var handler = new HttpClientHandler() { CookieContainer = cookies };
+                using var client = new HttpClient(handler) { BaseAddress = uri };
+                using var response = await client.GetAsync($"/2022/day/{day}/input");
+                using var stream = await response.Content.ReadAsStreamAsync();
+                await stream.CopyToAsync(file);
+            }
+
+            return filePath;
+        }
+
+        public static string GetRelativePath(int day, string filename)
+        {
+            var assemblyPath = AppDomain.CurrentDomain.BaseDirectory;
+            return Path.Combine(assemblyPath.Remove(assemblyPath.IndexOf(@"\bin\Debug")), "0" + day.ToString(), filename);
+        }
+
+        public static string GetCookie()
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "secrets");
+            return File.ReadAllLines(path).First();
+        }
+    }
+}
