250f06b6c49ee73c9766095fdcd8b949fb0efaf1
kmoroz <ksmorozova@hotmail.com>
2022-12-04 12:35:43 +0000

    add day 4

---
+++ AoC-2022/04/1.cs
@@ -0,0 +1,28 @@
+﻿var path = await Utils.GetInput(4);
+
+int runningSum = 0;
+foreach(var line in File.ReadLines(path))
+{
+    List<int> ranges = line.Split(',','-').Select(int.Parse).ToList<int>();
+    var firstSection = Enumerable.Range(ranges[0], ranges[1] - ranges[0] + 1);
+    var secondSection = Enumerable.Range(ranges[2], ranges[3] - ranges[2] + 1);
+    if (secondSection.All(i => firstSection.Contains(i)) 
+        || firstSection.All(i => secondSection.Contains(i)))
+        runningSum += 1;
+}
+
+//first part
+Console.WriteLine(runningSum);
+
+runningSum = 0;
+foreach (var line in File.ReadLines(path))
+{
+    List<int> ranges = line.Split(',', '-').Select(int.Parse).ToList<int>();
+    var firstSection = Enumerable.Range(ranges[0], ranges[1] - ranges[0] + 1);
+    var secondSection = Enumerable.Range(ranges[2], ranges[3] - ranges[2] + 1);
+    if (secondSection.Any(i => firstSection.Contains(i)))
+        runningSum += 1;
+}
+
+//second part
+Console.WriteLine(runningSum);
