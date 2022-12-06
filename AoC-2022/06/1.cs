85d74e3abae70f0a10f4ebb3f03524ded361d648
kmoroz <ksmorozova@hotmail.com>
2022-12-06 10:44:45 +0000

    add day 6

---
+++ AoC-2022/06/1.cs
@@ -0,0 +1,33 @@
+﻿var path = await Utils.GetInput(6);
+
+var datastreamBuffer = File.ReadAllText(path);
+int packetLength = 4;
+int messageLength = 14;
+
+Console.WriteLine(findMarker());
+Console.WriteLine(findMessage());
+
+
+int findMarker()
+{
+    for (int i = 0; i < datastreamBuffer.Length; i++)
+    {
+        List<char> fourChars = datastreamBuffer.Substring(i, packetLength).ToList();
+        bool areUnique = fourChars.Distinct().Count() == fourChars.Count();
+        if (areUnique)
+            return i + packetLength;
+    }
+    return 0;
+}
+
+int findMessage()
+{
+    for (int i = 0; i < datastreamBuffer.Length; i++)
+    {
+        List<char> fourChars = datastreamBuffer.Substring(i, messageLength).ToList();
+        bool areUnique = fourChars.Distinct().Count() == fourChars.Count();
+        if (areUnique)
+            return i + messageLength;
+    }
+    return 0;
+}
