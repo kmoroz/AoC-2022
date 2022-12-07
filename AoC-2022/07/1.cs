85c771062afd43d1f5d19fa301d74a256393e5a4
kmoroz <ksmorozova@hotmail.com>
2022-12-07 17:45:30 +0000

    add day7

---
+++ AoC-2022/07/1.cs
@@ -0,0 +1,74 @@
+﻿using AoC_2022._07;
+
+var path = await Utils.GetInput(7);
+var output = File.ReadAllText(path).Split('\n').SkipLast(1).ToArray();
+var lastLine = output.Last();
+var rootDir = new AOCDirectory("/", null);
+
+const string CD = "cd";
+const string LS = "ls";
+const string DIR = "dir";
+
+recordAOCDirectory(rootDir, output[1..]);
+
+AOCDirectory recordAOCDirectory(AOCDirectory currentDir, string[] output)
+{
+    if (output.Length != 0)
+    {
+        var tokens = output.First().Split(" ");
+        if (tokens.Contains(CD))
+        {
+            if (currentDir.Children.ContainsKey(tokens.Last()))
+                return recordAOCDirectory(currentDir.Children[tokens.Last()], output[1..]);
+            if (tokens.Last() == "..")
+                return recordAOCDirectory(currentDir.Parent, output[1..]);
+        }
+        else if (tokens.Contains(DIR))
+            currentDir.Push(new AOCDirectory(tokens.Last(), currentDir));
+        else if (!tokens.Contains(LS))
+            currentDir.Push(tokens.Last(), int.Parse(tokens.First()));
+        return recordAOCDirectory(currentDir, output[1..]);
+    }
+    return rootDir;
+}
+
+
+var smallDirs = new List<int>();
+FindSmallerDirs(rootDir);
+
+List<int> FindSmallerDirs(AOCDirectory dir)
+{
+    if (dir.Size <= 100000)
+        smallDirs.Add(dir.Size);
+    if (dir.Children.Any())
+    {
+        foreach (var child in dir.Children.Values)
+            FindSmallerDirs(child);
+    }
+    return smallDirs;
+}
+
+//part one
+Console.WriteLine(smallDirs.Sum());
+
+const int TotalDiscSpace = 70000000;
+const int SpaceRequired = 30000000;
+int unusedSpace = TotalDiscSpace - rootDir.Size;
+int minAmountToFreeUp = SpaceRequired - unusedSpace;
+var candidatesForDeletion = new List<int>();
+FindCandidatesForDeletion(rootDir);
+
+List<int> FindCandidatesForDeletion(AOCDirectory dir)
+{
+    if (dir.Size >= minAmountToFreeUp)
+        candidatesForDeletion.Add(dir.Size);
+    if (dir.Children.Any())
+    {
+        foreach (var child in dir.Children.Values)
+            FindCandidatesForDeletion(child);
+    }
+    return candidatesForDeletion;
+}
+
+//part two
+Console.WriteLine(candidatesForDeletion.Min());
---
+++ AoC-2022/07/AOCDirectory.cs
@@ -0,0 +1,36 @@
+﻿namespace AoC_2022._07
+{
+    public class AOCDirectory
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Size { get; set; } = 0;
+        public IDictionary<string, AOCDirectory> Children = new Dictionary<string, AOCDirectory>();
+        public AOCDirectory Parent;
+        public IDictionary<string, int> Files = new Dictionary<string, int>();
+
+        public AOCDirectory(string name, AOCDirectory parent)
+        {
+            Name = name;
+            Parent = parent;
+        }
+        public void Push(AOCDirectory child)
+        {
+            Children.Add(new KeyValuePair<string, AOCDirectory>(child.Name, child));
+        }
+
+        public void Push(string fileName, int fileSize)
+        {
+            Files.Add(new KeyValuePair<string, int>(fileName, fileSize));
+            Size += fileSize;
+            UpdateParent(Parent, fileSize);
+        }
+
+        private AOCDirectory UpdateParent(AOCDirectory parent, int size)
+        {
+            if (parent is null)
+                return null;
+            parent.Size += size;
+            return UpdateParent(parent.Parent, size);
+        }
+    }
+}
